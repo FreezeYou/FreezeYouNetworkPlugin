e855f99dd32f926d9f96dcfa87da7f243819d3d9
Playhi <000902play@gmail.com>
2022-03-15 14:48:39 +0800

    [WIP]

--- app/src/main/java/net/zidon/networkplugin/ui/FreezeYouNetworkPluginApp.kt
+++ app/src/main/java/net/zidon/networkplugin/ui/FreezeYouNetworkPluginApp.kt
@@ -1,14 +1,17 @@
 package net.zidon.networkplugin.ui
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import net.zidon.networkplugin.Screen
+import net.zidon.networkplugin.ui.account.AccountComponent
 import net.zidon.networkplugin.ui.theme.FreezeYouNetworkPluginTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,7 +39,8 @@ fun FreezeYouNetworkPluginApp() {
             }) {
             NavHost(
                 navController = appState.navController,
-                startDestination = Screen.Home.route
+                startDestination = Screen.Home.route,
+                modifier = Modifier.padding(it)
             ) {
                 composable(Screen.Home.route) { Text(text = "123") }
                 composable(Screen.Account.route) { AccountComponent() }
--- app/src/main/java/net/zidon/networkplugin/ui/account/AccountComponent.kt
+++ app/src/main/java/net/zidon/networkplugin/ui/account/AccountComponent.kt
@@ -1,9 +1,13 @@
-package net.zidon.networkplugin.ui
+package net.zidon.networkplugin.ui.account
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material.icons.rounded.Login
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,9 +17,45 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AccountComponent() {
-    LoginTable()
+    val scroll = rememberScrollState(0)
+    Column(
+        modifier = Modifier.verticalScroll(scroll)
+    ) {
+        ElevatedCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .height(160.dp)
+            ) {
+            }
+        }
+        ElevatedCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+                .padding(bottom = 8.dp)
+        ) {
+            Column(modifier = Modifier.padding(4.dp), horizontalAlignment = Alignment.Start) {
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { }) {
+                    Row(modifier = Modifier
+                    .padding(12.dp)) {
+                        Icon(Icons.Rounded.Settings, "")
+                        Spacer(modifier = Modifier.size(16.dp))
+                        Text("Settings")
+                    }
+                }
+            }
+        }
+    }
 }
 
 /**
@@ -24,7 +64,8 @@ fun AccountComponent() {
 @Composable
 fun LoginTable(username: String? = null) {
     Surface(
-        modifier = Modifier.fillMaxSize()){
+        modifier = Modifier.fillMaxSize()
+    ) {
         Column(
             verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally
